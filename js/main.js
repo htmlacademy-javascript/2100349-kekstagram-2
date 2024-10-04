21419baf9730e675099716c2ad202bfa4916f0e6
Aleksandr Anokhin <aleksandr0788@gmail.com>
2024-10-04 20:44:01 +0300

    Добавил массив названий для фото, настроил Неповторяемость id

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+const PHOTO_COUNT = 25;
 
-const MESSAGE_TEXT = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'У моего кота получилась фотография лучше.',
+];
+
+const DESCRIPTIONS = [
+  'Утро!',
+  'Котик',
+  'Солнышко',
+  'Красивое фото',
+  'Что-то новенькое',
 ];
 
 const NAMES = [
@@ -19,23 +23,45 @@ const NAMES = [
   'Юлия',
 ];
 
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const counter = () => {
+  let sum = 0;
+  return () => {
+    sum = sum + 1;
+    return sum;
+  };
+};
+
+const uniquePhoto = counter();
+const uniqueId = counter();
 const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
 
 const createComments = () => ({
   id: getRandomIntInclusive(0, 30),
-  avatar: 'img/avatar-'+ getRandomIntInclusive(1, 6) + '.svg',
-  message: getRandomArrayElement(MESSAGE_TEXT),
+  avatar: `img/avatar-${getRandomIntInclusive(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
 
-const createPhotoObjects = () => ({
-  id: getRandomIntInclusive(1, 25),
-  url: 'photos/' + getRandomIntInclusive(1, 25) + '.jpg',
-  description: 'Новая фотография',
-  likes: getRandomIntInclusive(15, 200),
-  comments: createComments(),
-});
 
-console.log (
-  createPhotoObjects()
+const createPhoto = () => {
+  const id = uniqueId();
+  return {
+    id: id,
+    url: `photos/${uniquePhoto()}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomIntInclusive(15, 200),
+    comments: Array.from({ length: getRandomIntInclusive(0, 30) }, createComments),
+  };
+};
+
+const createPhotos = () => Array.from({ length: PHOTO_COUNT }, createPhoto);
+
+console.log(
+  createPhotos()
 );
