5de3edd601f08a5d0381cd9f0cead642ac7a1ce9
Aleksandr Anokhin <aleksandr0788@gmail.com>
2024-11-15 18:32:31 +0300

    start homework 7

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,3 +1,15 @@
+const containerElement = document.querySelector('.pictures');
+const cardTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+
 export const renderCards = (photos) => {
   console.log(photos);
+const fragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    const thumbnail = cardTemplate.cloneNode(true);
+    thumbnail.querySelector('.picture_img').src = photo.url;
+    thumbnail.querySelector('.picture_comments').data.textContent('comments'.length);
+    thumbnail.querySelector('#likes');
+  });
+  containerElement.append(fragment);
 };
